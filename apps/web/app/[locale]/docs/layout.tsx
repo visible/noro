a80ae784c5e89fdc596bc882b5cb0f3b44edbe25
josh <josh@afterima.ge>
2026-01-26 17:32:00 +0000

    fix: move sidebar script after sidebar element

--- apps/web/app/[locale]/docs/layout.tsx
+++ apps/web/app/[locale]/docs/layout.tsx
@@ -21,7 +21,6 @@ export default function DocsLayout({
 }: { children: React.ReactNode }) {
 	return (
 		<div className="h-screen bg-[#F5F3EF] flex flex-col overflow-hidden">
-			<script dangerouslySetInnerHTML={{ __html: sidebarscript }} />
 
 			<header className="shrink-0 bg-transparent">
 				<div className="flex items-center justify-between py-6 px-6 md:px-12">
@@ -34,6 +33,7 @@ export default function DocsLayout({
 				<div className="flex-1 flex bg-white/90 backdrop-blur-sm rounded-2xl overflow-hidden shadow-lg shadow-black/5 border border-black/5 relative z-0">
 					<div className="absolute -left-1 top-1/3 w-1 h-24 bg-[#C53D43] rounded-full hidden lg:block" />
 					<Sidebar />
+					<script dangerouslySetInnerHTML={{ __html: sidebarscript }} />
 					<main className="flex-1 min-w-0 px-6 md:px-8 lg:px-12 overflow-y-auto [scrollbar-width:none] [-webkit-overflow-scrolling:touch] [&::-webkit-scrollbar]:hidden">
 						{children}
 					</main>
